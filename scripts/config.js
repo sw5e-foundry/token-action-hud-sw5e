23b33472e883ab586393acd41b05d1a0f7bae2f9
Russell Larkin <russell.larkin@gmail.com>
2023-01-26 03:12:18 +0000

    Ready for distribution - Make ready for distribution.

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 // For distribution
-/* const coreModulePath = '../../token-action-hud-core/scripts/token-action-hud-core.min.js'
+const coreModulePath = '../../token-action-hud-core/scripts/token-action-hud-core.min.js'
 const coreModule = await import(coreModulePath)
 export const CoreActionHandler = coreModule.ActionHandler
 export const CoreActionListExtender = coreModule.ActionListExtender
@@ -8,10 +8,10 @@ export const CorePreRollHandler = coreModule.PreRollHandler
 export const CoreRollHandler = coreModule.RollHandler
 export const CoreSystemManager = coreModule.SystemManager
 export const CoreUtils = coreModule.Utils
-export const Logger = coreModule.Logger */
+export const Logger = coreModule.Logger
 
 // For development
-const coreModulePath = '../../token-action-hud-core/'
+/* const coreModulePath = '../../token-action-hud-core/'
 const coreActionHandlerFile = `${coreModulePath}scripts/action-handlers/action-handler.js`
 const coreActionListExtenderFile = `${coreModulePath}scripts/action-handlers/action-list-extender.js`
 const coreCategoryManagerFile = `${coreModulePath}scripts/category-manager.js`
@@ -28,4 +28,4 @@ export const CoreRollHandler = await import(coreRollHandlerFile).then(module => 
 export const CoreSystemManager = await import(coreSystemManagerFile).then(module => module.SystemManager)
 const coreUtilsModule = await import(coreUtilsFile)
 export const CoreUtils = coreUtilsModule.Utils
-export const Logger = coreUtilsModule.Logger
+export const Logger = coreUtilsModule.Logger */
